1d0131a973dbaac2614bd0305a567682bbd274ad
Joaquin Rodriguez <joaquin@outlook.com>
2020-05-27 10:56:04 -0500

    Fixed small typo in comment

--- src/interfaces/ftmarker.interface.ts
+++ src/interfaces/ftmarker.interface.ts
@@ -1,7 +1,7 @@
 import { data } from 'azure-maps-control';
 
 /**
- * @desc An interface for storing the metadata for reach food truck.
+ * @desc An interface for storing the metadata for each food truck.
  */
 export declare interface FTMarker {
     coordinates: data.Position;
